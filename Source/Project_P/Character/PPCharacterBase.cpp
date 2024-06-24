4f57dd979b872d1bb9df1665049d04f785a6178b
KimJunSeo <108670965+k99812@users.noreply.github.com>
2024-06-24 17:21:07 +0900

    2024.06.24

--- Source/Project_P/Character/PPCharacterBase.cpp
+++ Source/Project_P/Character/PPCharacterBase.cpp
@@ -2,33 +2,22 @@
 
 
 #include "Character/PPCharacterBase.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/MovementComponent.h"
 
 // Sets default values
 APPCharacterBase::APPCharacterBase()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	//폰 설정
+	//컨트롤러의 회전(Pitch, Roll, Yaw) 값을 적용할지
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
 
+	//캡슐 콜라이더 설정
+	//추후 사이즈 조정해야됨
+	GetCapsuleComponent()->InitCapsuleSize(42.0f, 96.0f);
+	GetCapsuleComponent()->SetCollisionProfileName(TEXT("Pawn"));
 }
 
-// Called when the game starts or when spawned
-void APPCharacterBase::BeginPlay()
-{
-	Super::BeginPlay();
-	
-}
-
-// Called every frame
-void APPCharacterBase::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
-// Called to bind functionality to input
-void APPCharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
-}
 
