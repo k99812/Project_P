65ab459ac027e86fd26f86af9805fbb2f7f7be5e
KimJunSeo <108670965+k99812@users.noreply.github.com>
2024-07-03 17:27:50 +0900

    2024.07.03

--- Source/Project_P/Character/PPGASCharacterPlayer.cpp
+++ Source/Project_P/Character/PPGASCharacterPlayer.cpp
@@ -13,7 +13,7 @@
 
 APPGASCharacterPlayer::APPGASCharacterPlayer()
 {
-	//스켈레텔메쉬 설정
+//스켈레텔메쉬 설정
 	static ConstructorHelpers::FObjectFinder<USkeletalMesh> CharacterMeshRef(TEXT("/Script/Engine.SkeletalMesh'/Game/ParagonCountess/Characters/Heroes/Countess/Meshes/SM_Countess.SM_Countess'"));
 	if (CharacterMeshRef.Object)
 	{
@@ -21,7 +21,7 @@ APPGASCharacterPlayer::APPGASCharacterPlayer()
 		GetMesh()->SetSkeletalMesh(CharacterMeshRef.Object);
 	}
 
-	//애니메이션인스턴스클래스 설정
+//애니메이션인스턴스클래스 설정
 	static ConstructorHelpers::FClassFinder<UAnimInstance> AnimInstanceClassRef(TEXT(""));
 	if (AnimInstanceClassRef.Class)
 	{
@@ -29,7 +29,7 @@ APPGASCharacterPlayer::APPGASCharacterPlayer()
 		GetMesh()->SetAnimInstanceClass(AnimInstanceClassRef.Class);
 	}
 
-	//카메라 암(USpringArmComponent) 설정
+//카메라 암(USpringArmComponent) 설정
 	CameraArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraArm"));
 	CameraArm->SetupAttachment(RootComponent);
 	//캐릭터와 카메라의 거리
@@ -37,14 +37,14 @@ APPGASCharacterPlayer::APPGASCharacterPlayer()
 	//로테이션을 컨트롤러의 로테이션과 동기화 할지
 	CameraArm->bUsePawnControlRotation = true;
 
-	//카메라 설정()
+//카메라 설정()
 	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
 	//카메라를 카메라암에 자식으로 붙임
 	FollowCamera->SetupAttachment(CameraArm, USpringArmComponent::SocketName);
 	//로테이션을 컨트롤러의 로테이션과 동기화 할지
 	FollowCamera->bUsePawnControlRotation = false;
 
-	//인풋 설정
+//인풋 설정
 	static ConstructorHelpers::FObjectFinder<UInputMappingContext> InputMappingContextRef(TEXT("/Script/EnhancedInput.InputMappingContext'/Game/Project_P/Input/IMC_Third.IMC_Third'"));
 	if (InputMappingContextRef.Object)
 	{
@@ -85,7 +85,7 @@ void APPGASCharacterPlayer::PossessedBy(AController* NewController)
 		ASC = GASPlayerState->GetAbilitySystemComponent();
 		if (ASC)
 		{
-			PPGAS_LOG(LogPPGAS, Log, TEXT("ASC is successed"));
+			//PPGAS_LOG(LogPPGAS, Log, TEXT("ASC is successed"));
 			
 			ASC->InitAbilityActorInfo(GASPlayerState, this);
 		}
