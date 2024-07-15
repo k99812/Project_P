4173a9a9647a2da9cf46dbfb50537b1bb8dc30f0
KimJunSeo <108670965+k99812@users.noreply.github.com>
2024-07-15 17:28:37 +0900

    2024.07.15

---
+++ Source/Project_P/Animation/PPAnimInstance.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Animation/PPAnimInstance.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+UPPAnimInstance::UPPAnimInstance()
+{
+	MovingThreshould = 3.0f;
+	JumpingThreshould = 100.0f;
+}
+
+void UPPAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	//AnimInstance가 생성될때 한번만 호출되어 포인터 변수들을 초기화할때 사용
+	Owner = Cast<ACharacter>(GetOwningActor());
+	if (Owner)
+	{
+		Movement = Owner->GetCharacterMovement();
+	}
+}
+
+void UPPAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (Movement)
+	{
+		Velocity = Movement->Velocity;
+		Direction = CalculateDirection(Velocity, Owner->GetActorRotation());
+		//Z축(하늘 방향)을 제외한 X, Y축의 크기를 반환함
+		GroundSpeed = Velocity.Size2D();
+		bIsIdle = GroundSpeed < MovingThreshould;
+		bIsFalling = Movement->IsFalling();
+		bIsJumping = bIsFalling & (Velocity.Z > JumpingThreshould);
+	}
+}
---
+++ Source/Project_P/Animation/PPAnimInstance.h
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "PPAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_P_API UPPAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+public:
+	UPPAnimInstance();
+
+protected:
+	//AnimInstance가 생성될때 한번만 호출
+	virtual void NativeInitializeAnimation() override;
+
+	//Tick마다 호출
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Character)
+	TObjectPtr<class ACharacter> Owner;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Character)
+	TObjectPtr<class UCharacterMovementComponent> Movement;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character)
+	FVector Velocity;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character)
+	float Direction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character)
+	float GroundSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character)
+	uint8 bIsIdle : 1;
+
+	//움직이는지 검사하기 위한 변수 GroundSpeed < MovingThreshould
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character)
+	float MovingThreshould;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character)
+	uint8 bIsFalling : 1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character)
+	uint8 bIsJumping : 1;
+
+	//점프하는지 검삭하기 위한 변수
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character)
+	float JumpingThreshould;
+};
